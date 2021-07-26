eaa340de93f87c4959b59a468108f8537f1b2133
kembg <kennedymoises2@gmail.com>
2021-07-26 13:36:20 -0300

    Hello em springboot

---
+++ dadosSQL/db_generation_game_online_tb_classe.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `db_generation_game_online` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_generation_game_online`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_generation_game_online
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_classe`
+--
+
+DROP TABLE IF EXISTS `tb_classe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_classe` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `posicao` varchar(255) NOT NULL,
+  `dificuldade` varchar(255) NOT NULL,
+  `vantagem_sobre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_classe`
+--
+
+LOCK TABLES `tb_classe` WRITE;
+/*!40000 ALTER TABLE `tb_classe` DISABLE KEYS */;
+INSERT INTO `tb_classe` VALUES (1,'atirador','média','caçador'),(2,'caçador','difícil','suporte'),(3,'suporte','fácil','lutador'),(4,'lutador','média','tank'),(5,'tank','fácil','atirador');
+/*!40000 ALTER TABLE `tb_classe` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-26  9:01:14
---
+++ dadosSQL/db_generation_game_online_tb_personagem.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE  IF NOT EXISTS `db_generation_game_online` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_generation_game_online`;
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_generation_game_online
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_personagem`
+--
+
+DROP TABLE IF EXISTS `tb_personagem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_personagem` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(255) NOT NULL,
+  `poder_ataque` bigint DEFAULT NULL,
+  `poder_defesa` bigint DEFAULT NULL,
+  `raridade` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `classe_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `classe_id` (`classe_id`),
+  CONSTRAINT `tb_personagem_ibfk_1` FOREIGN KEY (`classe_id`) REFERENCES `tb_classe` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_personagem`
+--
+
+LOCK TABLES `tb_personagem` WRITE;
+/*!40000 ALTER TABLE `tb_personagem` DISABLE KEYS */;
+INSERT INTO `tb_personagem` VALUES (1,'humano',3000,2000,'raro','Rei Caído',2),(2,'elfo',4000,1000,'comum','Loiro cego',1),(3,'anão',1000,4000,'raro','Anão Manco',3),(4,'humano',3500,2000,'raro','Punho surdo',4),(5,'ogro',5000,2000,'mítico','Pele de aço',5),(6,'ogro',3000,2000,'mítico','Olho feroz',1),(7,'elfo',4000,2000,'lendário','Orelha dolorosa',2),(8,'ogro',1500,4000,'raro','Cajado gentil',3);
+/*!40000 ALTER TABLE `tb_personagem` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-26  9:01:14
